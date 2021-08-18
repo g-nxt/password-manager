714a7909437d23aa7914d2ff55dd21ebf13941ac
San <santhoshjke@hotmail.com>
2021-08-17 23:32:24 -0500

    Updates

---
+++ precache-manifest.0cb7fa8cb84158d62f5e6c78827c2a8b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e9601f478622b703cd513d02572a88",
+    "url": "/password-manager/index.html"
+  },
+  {
+    "revision": "b599f3878d1fe7ed1ae0",
+    "url": "/password-manager/static/css/main.272aab2a.chunk.css"
+  },
+  {
+    "revision": "4d46b0621cc9dcbf6193",
+    "url": "/password-manager/static/js/2.2124d0e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/password-manager/static/js/2.2124d0e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b599f3878d1fe7ed1ae0",
+    "url": "/password-manager/static/js/main.babd2249.chunk.js"
+  },
+  {
+    "revision": "3fb4159367189c3f6253",
+    "url": "/password-manager/static/js/runtime-main.a405ae8f.js"
+  },
+  {
+    "revision": "8b1855b439b4fa653f45ab31ef6b2b6a",
+    "url": "/password-manager/static/media/add-icon.8b1855b4.png"
+  },
+  {
+    "revision": "fc34633e57880a3c6b6835184a816eac",
+    "url": "/password-manager/static/media/delete-icon.fc34633e.png"
+  },
+  {
+    "revision": "1256dd709406e1ef627f66c6d707c301",
+    "url": "/password-manager/static/media/edit-icon.1256dd70.png"
+  },
+  {
+    "revision": "faec13695b698dff12b409d435246f40",
+    "url": "/password-manager/static/media/eye.faec1369.png"
+  },
+  {
+    "revision": "c7c06ab2cfc754b2462eaad67552ebcb",
+    "url": "/password-manager/static/media/footer-graphic.c7c06ab2.png"
+  }
+]);
